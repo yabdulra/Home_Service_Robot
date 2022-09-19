e49c13dc75dd6c402266549ae7a61afe1bf1bdcc
yabdulra <yusufjauro@gmail.com>
2022-09-19 10:07:43 -0600

    edit: deleted previous shell scripts

--- test_navigation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-xterm  -e  " source /opt/ros/melodic/setup.bash; roscore" & 
-sleep 2
-xterm  -e "roslaunch turtlebot3_gazebo turtlebot3_world.launch" &
-sleep 5
-xterm  -e "roslaunch service_bot amcl.launch" &
-sleep 5
-xterm  -e "roslaunch turtlebot3_gazebo view_navigation.launch" 
