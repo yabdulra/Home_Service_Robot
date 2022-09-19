f1deead84413c0a4265b044a4fc764183325f139
yabdulra <yusufjauro@gmail.com>
2022-09-18 22:01:34 -0600

    edit: source workspave edit

--- test_navigation.sh
+++ test_navigation.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
+xterm  -e  " source /opt/ros/melodic/setup.bash; roscore" & 
 sleep 2
 xterm  -e "roslaunch turtlebot3_gazebo turtlebot3_world.launch" &
 sleep 5
